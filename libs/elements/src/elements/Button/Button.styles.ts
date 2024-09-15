fb38b6ff904e9161223d615da83b2f51c6b3f297
Renato Pozzi <askides@proton.me>
2024-09-15 21:17:48 +0200

    feat(button): add ring

--- libs/elements/src/elements/Button/Button.styles.ts
+++ libs/elements/src/elements/Button/Button.styles.ts
@@ -1,11 +1,11 @@
 import { tv } from 'tailwind-variants';
 
 export const styles = tv({
-  base: 'inline-flex items-center gap-x-2 font-medium rounded-lg border focus:outline-none disabled:opacity-50 disabled:pointer-events-none',
+  base: 'inline-flex items-center gap-x-2 font-medium rounded-lg border focus:outline-none disabled:opacity-50 disabled:pointer-events-none focus:ring-2 focus:ring-offset-2',
   variants: {
     art: {
-      ichi: 'border-transparent bg-emerald-500 text-white shadow-sm hover:bg-emerald-600 focus:bg-emerald-600 active:shadow-inner',
-      ni: 'border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:bg-gray-50 active:shadow-inner',
+      ichi: 'border-transparent bg-emerald-500 text-white shadow-sm hover:bg-emerald-600 active:shadow-inner',
+      ni: 'border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-zinc-100 active:shadow-inner',
     },
     mass: {
       sm: 'px-2 py-1 text-xs',
