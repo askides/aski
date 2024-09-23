bda620069cd3543da0ed27c17c8740d02e7a1d1e
Renato Pozzi <askides@proton.me>
2024-09-24 01:19:55 +0200

    feat: add paths on vite

--- apps/with-react/vite.config.ts
+++ apps/with-react/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '~elements': resolve(__dirname, 'src', 'elements'),
+    },
+  },
+});
