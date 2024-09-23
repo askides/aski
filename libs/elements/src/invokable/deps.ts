d11c26aeb3011b96cb4776694c9a08db9b43d676
Renato Pozzi <askides@proton.me>
2024-09-24 00:57:02 +0200

    feat: change elements deps

--- libs/elements/src/invokable/deps.ts
+++ libs/elements/src/invokable/deps.ts
@@ -9,15 +9,23 @@ import Pkg from '../../package.json';
 const traverse = _traverse.default;
 
 (async () => {
+  // Get directory from arguments.
+  const [, , ...args] = process.argv;
+
+  if (args.length < 1) {
+    throw new Error('Please provide a directory to scan.');
+  }
+
+  // Define the path to the diretory to scan.
+  const dirPath = path.join('src', args[0]);
+
   // Scan all the dirs in src/elements
-  const dirs = await readdir(path.join(process.cwd(), 'src', 'elements'));
+  const dirs = await readdir(path.join(process.cwd(), dirPath));
 
   // Get all the .tsx files in all the dirs
   const files = await Promise.all(
     dirs.map(async (dir) => {
-      const files = await readdir(
-        path.join(process.cwd(), 'src', 'elements', dir),
-      );
+      const files = await readdir(path.join(process.cwd(), dirPath, dir));
 
       return files
         .filter((el) => el.endsWith('.tsx'))
@@ -26,7 +34,7 @@ const traverse = _traverse.default;
   ).then((files) => files.flat());
 
   // Join the dir and file name
-  const filesWithDirs = files.map((file) => path.join('src', 'elements', file));
+  const filesWithDirs = files.map((file) => path.join(dirPath, file));
 
   // Read the content of each file & extract the dependencies.
   for (const file of filesWithDirs) {
@@ -52,24 +60,35 @@ const traverse = _traverse.default;
 
     // Obtain the currently installed version of the dependencies.
     const depsWithVersion = new Map<string, string>();
+    const depsInElements = new Set<string>();
     const depsInPkg = (Pkg.dependencies || {}) as Record<string, string>;
 
     for (const dep of deps) {
       if (depsInPkg && depsInPkg[dep]) {
         depsWithVersion.set(dep, depsInPkg[dep]);
       }
+
+      // Check if the dependency is in the elements package.
+      if (dep.startsWith('~elements/')) {
+        depsInElements.add(dep.replace('~elements/', ''));
+      }
     }
 
     const name = file.split('/').pop();
     const dependencies = Object.fromEntries(depsWithVersion);
+    const elements = Array.from(depsInElements);
 
     console.log('Element ->', name);
     console.log('Dependencies ->', dependencies);
+    console.log('Elements ->', elements);
 
     // Write a '.dependencies.json' file with the dependencies.
     const depsPath = file.replace('.tsx', '.dependencies.json');
 
-    await writeFile(depsPath, JSON.stringify(dependencies, null, 2));
+    await writeFile(
+      depsPath,
+      JSON.stringify({ dependencies, elements }, null, 2),
+    );
 
     console.log('Dependencies file written to ->', depsPath, '\n');
   }
